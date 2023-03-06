a3bba8a13b011187faad367649f96902f906a1d6
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-06 18:00:37 +0000

    added bcrypt info

--- middleWareExample.js
+++ middleWareExample.js
@@ -15,8 +15,6 @@ const middleOne = async (req, res, next) => {
 // "next()" - express is expecting a third (optional) argument called next,
 // this will pass the request onto the next function once stuff has happened
 
-
-
 const middleTwo = async ( req, res, next) => {
     console.log("start middleTwo", req.body);
     req.body["middleTwo"] = "Im from the middleTwo";
---
+++ src/users/controllers.js
@@ -0,0 +1,33 @@
+const User = require("./model");
+
+const registerUser = async (req, res) => {
+    try {
+        
+        const user = await User.create (req.body);
+
+        // const user = await User.create({
+        //     username: req.body.username,
+        //     email: req.body.email,
+        //     password: req.body.password,
+        // });
+
+        
+        res.status(201).json({ message: "Success", user: {username: req.body.username, email: req.body.email }, 
+    });
+    } catch (error) {
+        res.status(501).json({ errormessage: error.message, error: error });
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        res.status(201).json({ message: "success", user: {username:req.user.email}});
+    } catch (error) {
+        res.status(501).json({ errormessage: error.message, error: error });
+    }
+}
+
+module.exports = {
+    registerUser,
+    login
+};
